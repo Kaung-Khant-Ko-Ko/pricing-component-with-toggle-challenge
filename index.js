ff4e30cf3f990fb2f0d02e6e7fb2882d9158d651
Kaung Khant Ko Ko <59255928+Kaung-Khant-Ko-Ko@users.noreply.github.com>
2024-06-29 18:29:53 +0630

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 $("#plan").on({
   input: function () {
     if ($(this).is(":checked")) {
-      $("#price_basic").text("199.99");
-      $("#price_professional").text("249.99");
-      $("#price_master").text("399.99");
-    } else {
       $("#price_basic").text("19.99");
       $("#price_professional").text("24.99");
       $("#price_master").text("39.99");
+    } else {
+      $("#price_basic").text("199.99");
+      $("#price_professional").text("249.99");
+      $("#price_master").text("399.99");
     }
   },
 });
